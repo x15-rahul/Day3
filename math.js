c8c59d285a10a55579f247d139540f15b3baf35b
x15-rahul <biswasrahul97@outlook.com>
2023-03-30 10:44:33 +0600

    math function is added

---
+++ math.js
@@ -0,0 +1,18 @@
+const yourNumber = 45;
+const herNumber = 56;
+
+/* if(Math.abs((yourNumber - herNumber)) <= 5){
+    console.log('She can be your friend');
+} else {
+    console.log('She can not be your friend');
+} */
+
+// console.log(Math.pow(herNumber, 3));
+
+const number = 3.01;
+// console.log(Math.round(number));
+// console.log(Math.ceil(number));
+
+for(let i = 1; i <= 10; i++){
+    console.log(Math.round(Math.random() * 10));
+}
