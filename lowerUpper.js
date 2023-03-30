c2bd890d38bf2281b41a932ddae40604a6a847f2
x15-rahul <biswasrahul97@outlook.com>
2023-03-30 10:22:34 +0600

    toLowerCase & toUpperCase are added

---
+++ lowerUpper.js
@@ -0,0 +1,8 @@
+const userName = 'rahulbiswas';
+const givenName = 'RahulBiswas';
+
+if(userName.toUpperCase() === givenName.toUpperCase()){
+    console.log('user valid');
+} else {
+    console.log('user not found');
+}
